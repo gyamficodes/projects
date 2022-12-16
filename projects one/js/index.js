4fdb4450c448e596d9f86ae8cbf6b9597a29696a
gyamficodes <gyamfijohn08@gmail.com>
2022-12-16 13:41:19 +0000

    project update

--- projects one/js/index.js
+++ projects one/js/index.js
@@ -1,19 +1,44 @@
 
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+// const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
-  // console.log(randomNumber);
+// btn.addEventListener("click", function () {
+//   const randomNumber = getRandomNumber();
+//   // console.log(randomNumber);
 
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
-});
+//   document.body.style.backgroundColor = colors[randomNumber];
+//   color.textContent = colors[randomNumber];
+// });
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
+// function getRandomNumber() {
+//   return Math.floor(Math.random() * colors.length);
+// }
+
+
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", 'orange','blue'];
+const color = document.querySelector('.color')
+
+
+document.getElementById('btn').onclick = function(){
+const randomNumber = getRandomnumber();
+console.log(randomNumber)
+
+document.body.style.backgroundColor = colors[randomNumber]
+color.textContent = colors[randomNumber];
 }
+
+function getRandomnumber(){
+  return Math.floor(Math.random() * colors.length)
+}
+
+
+
+
+
+
+
 
 
 
