2bbad7ac89ea469ea6996704c171ccc40a21a8b4
gyamficodes <gyamfijohn08@gmail.com>
2022-12-24 21:07:32 +0000

    lesson

---
+++ try/index.js
@@ -0,0 +1,22 @@
+
+const navtoggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('linsk  ')
+
+
+navtoggle.addEventListener('click') , function(){
+    console.log(links.classList)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
